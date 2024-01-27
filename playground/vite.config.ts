579662d5dd7071bb4849909019ba6fb0aeb5028e
Sean Boult <996134+Hacksore@users.noreply.github.com>
2024-01-27 10:46:33 -0600

    STILL WIP™

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import caddySsl from "../src/index.js";
-console.log("caddySsl", caddySsl);
 
 const config = defineConfig({
-  // @ts-ignore
-  plugins: [caddySsl()]
+  plugins: [caddySsl()],
 });
 
 export default config;
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import fs from "node:fs";
 import fsExtra from "fs-extra";
 
 import { execSync, spawn } from "child_process";
-import { convertCrtToPem } from "./cert-utils";
+// import { convertCrtToPem } from "./cert-utils";
 
 // TODO: Make this configurable
 const caddyfile = `myapp.localhost {
@@ -11,6 +11,9 @@ const caddyfile = `myapp.localhost {
   log stdout
   reverse_proxy localhost:5173
 }`;
+
+const cwd = process.cwd();
+const SSL_DIR = `${cwd}/.ssl`;
 
 // https://caddyserver.com/docs/conventions#data-directory
 // get the cert from here
@@ -28,11 +31,11 @@ export default function viteCaddySslPlugin(): Plugin {
         console.error("caddy cli is not installed");
       }
 
-      const cwd = process.cwd();
       // write this caddyfile to ssl directory
-      console.log("Writing caddyfile to", ".ssl/Caddyfile", cwd);
-      fsExtra.mkdirp(".ssl");
-      fs.writeFileSync(".ssl/Caddyfile", caddyfile);
+      console.log("Writing caddyfile to", SSL_DIR);
+
+      fsExtra.mkdirp(SSL_DIR);
+      fs.writeFileSync(`${SSL_DIR}/Caddyfile`, caddyfile);
 
       // run caddy cli to start a local server on port
       console.log("caddyInstalled", caddyInstalled);
@@ -48,19 +51,26 @@ export default function viteCaddySslPlugin(): Plugin {
         // console.error(`stderr: ${data}`);
       });
 
+      // print caddy file
+      console.log(caddyfile)
+
+      console.log("Access your app at https://myapp.localhost");
+
       // ~/Library/Application Support/Caddy
-      const certPath = `${process.env.HOME}/Library/Application Support/Caddy/certificates/local/myapp.localhost/myapp.localhost.crt`;
-      const certKey = `${process.env.HOME}/Library/Application Support/Caddy/certificates/local/myapp.localhost/myapp.localhost.key`;
+      // const certPath = `${process.env.HOME}/Library/Application Support/Caddy/certificates/local/myapp.localhost/myapp.localhost.crt`;
+      // const certKey = `${process.env.HOME}/Library/Application Support/Caddy/certificates/local/myapp.localhost/myapp.localhost.key`;
 
+      // const sslDir = `${cwd}/.ssl`;
+      // execSync(`openssl x509 -in "${certPath}" -out ${sslDir}/cert.pem -outform PEM`);
       // convertCrtToPem(certPath, `${cwd}/.ssl/cert.pem`);
-      fs.writeFileSync(`${cwd}/.ssl/cert.pem`, fs.readFileSync(certPath, "utf8"), "utf8");
-      fs.writeFileSync(`${cwd}/.ssl/cert.key`, fs.readFileSync(certKey, "utf8"), "utf8");
+      // fs.writeFileSync(`${cwd}/.ssl/cert.pem`, fs.readFileSync(certPath, "utf8"), "utf8");
+      // fs.writeFileSync(`${cwd}/.ssl/cert.key`, fs.readFileSync(certKey, "utf8"), "utf8");
 
-      const certificate = `${cwd}/.ssl/cert.pem`;
-      const certificateKey = `${cwd}/.ssl/cert.pem`;
-      const https = () => ({ cert: certificate, key: certificateKey });
-      config.server.https = Object.assign({}, config.server.https, https());
-      config.preview.https = Object.assign({}, config.preview.https, https());
+      // const certificate = `${cwd}/.ssl/cert.pem`;
+      // const certificateKey = `${cwd}/.ssl/cert.pem`;
+      // const https = () => ({ cert: certificate, key: certificate });
+      // config.server.https = Object.assign({}, config.server.https, https());
+      // config.preview.https = Object.assign({}, config.preview.https, https());
     }
   };
 }
